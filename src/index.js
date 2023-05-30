c5f5677c9ab1b53b886f5277f6b030a61127afcc
nzxf <122195155+nzxf@users.noreply.github.com>
2023-05-30 18:32:17 +0700

    Add and link style and image asset

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+import './style.css';
+import data from './data.json'
+import sample from './images/rest.gif'
 
-const elementMaker = (type, className, parent, text) => {
-    const element = document.createElement(type)
-    element.classList.add(className)
-    element.innerText = text
-    parent.appendChild(element)
-    return element
-}
+const elementMaker = (type, className, parent, text = "") => {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  element.innerText = text;
+  parent.appendChild(element);
+  return element;
+};
 
-const content = document.querySelector('.content')
-const test = elementMaker('div', 'test', content, "Helloooo!!")
+const content = document.querySelector('.content');
+const test = elementMaker('div', 'test', content, 'Helloooo!!');
+const testImg = elementMaker('img', 'test-img', content);
+testImg.src = sample
+
