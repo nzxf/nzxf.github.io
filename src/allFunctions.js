7076f64147c578df75fbda89bd3e400670aec782
nzxf <122195155+nzxf@users.noreply.github.com>
2023-06-19 20:37:12 +0700

    Redesign the page

--- src/allFunctions.js
+++ src/allFunctions.js
@@ -1,9 +1,13 @@
-const elementMaker = (type, className, parent, text = "") => {
-    const element = document.createElement(type);
-    element.classList.add(className);
-    element.innerText = text;
-    parent.appendChild(element);
-    return element;
+const myFunctions = (() => {
+  const getEl = (name) => document.querySelector(name);
+  const getEls = (arr) => document.querySelectorAll(arr.join(', '));
+  const makeEl = (type, className, parent) => {
+    const el = document.createElement(type);
+    el.classList.add(className);
+    parent.appendChild(el);
+    return el;
   };
+  return { makeEl, getEl, getEls };
+})();
 
-export { elementMaker }
+export { myFunctions };
--- src/index.js
+++ src/index.js
@@ -1,62 +1,20 @@
 import './main.css';
-import './navbar.css';
 import data from './data.json';
-import { elementMaker } from './allFunctions';
-import { makeNavbar } from './navbar';
+import { myFunctions } from './allFunctions';
 // import sample from './images/rest.gif'
 
-const content = document.querySelector('.content');
+const getEl = myFunctions.getEl;
+const makeEl = myFunctions.makeEl;
 
-makeNavbar(data.projects, content);
+const content = getEl('.content');
 
-const mainBody = elementMaker('div', 'main-body', content);
+console.log(data.projects.length)
 
-// PROJECT CONTAINER
-const fillMain = (array, parent) => {
-  for (let i = 0; i < array.length; i++) {
-    const projectContainerMain = elementMaker(
-      'div',
-      'project-container-main',
-      parent
-    );
-    // START
-    const preview = elementMaker('div', 'preview', projectContainerMain);
-    if (array[i].media == undefined) {
-      preview.style.backgroundImage = `url(${array[0].media.image})`;
-    } else {
-      preview.style.backgroundImage = `url(${array[i].media.image})`;
-    }
-    preview.addEventListener('mouseover', function () {
-      console.log('play videooooo');
-    });
-    // MIDDLE
-    const title = elementMaker(
-      'div',
-      'title',
-      projectContainerMain,
-      array[i].name.toUpperCase()
-    );
-    const description = elementMaker(
-      'div',
-      'description',
-      projectContainerMain,
-      array[i].description
-    );
+for (let i = 0; i < data.projects.length; i++) {
+    const container = makeEl('div', 'container', content)
+    container.style.backgroundImage = `url(${data.projects[i].media.image})`
 
-    // END
-    const containerEnd = elementMaker(
-      'div',
-      'container-end',
-      projectContainerMain
-    );
-
-    const live = elementMaker('a', 'live', containerEnd, 'live');
-    live.href = array[i].links.live;
-    live.style.display = 'block';
-    const code = elementMaker('a', 'code', containerEnd, 'code');
-    code.href = array[i].links.code;
-    code.style.display = 'block';
-  }
-};
-
-fillMain(data.projects, mainBody);
+    container.addEventListener('click', ()=>{
+        console.log(data.projects[i].links.live);
+    })
+}
