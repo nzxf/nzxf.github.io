aa028732f51bff650c9c81b90f3327802253348a
nzxf <122195155+nzxf@users.noreply.github.com>
2024-11-11 23:23:01 +0700

    Set output into static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nzxf.my.id',
+  site: 'https://nzxf.github.io',
+  output: 'static',
   integrations: [tailwind()],
 });
