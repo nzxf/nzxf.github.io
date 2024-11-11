b82162be668192b41d76b692bdf8f1173ea20353
nzxf <122195155+nzxf@users.noreply.github.com>
2024-11-11 22:06:13 +0700

    Add site and base in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nzxf.github.io',
+  base: 'my-repo',
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel(),
