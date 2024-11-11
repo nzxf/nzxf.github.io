0980fee5cac438ce771f67cdc2ef6c4e89c58296
nzxf <122195155+nzxf@users.noreply.github.com>
2024-11-11 23:04:44 +0700

    Remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/serverless';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nzxf.github.io',
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
 });
