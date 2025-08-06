b7f437c8d2fb6fd027afd98e52245143138c9bf2
nzxf <122195155+nzxf@users.noreply.github.com>
2025-08-06 13:05:52 +0700

    Add images to pricings and mezzanine

--- dist/chunks/kn001-bangun-rumah-minimalis_DtEIt_Ll.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./kn001-bangun-rumah-minimalis_Cp_nAEhN.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
